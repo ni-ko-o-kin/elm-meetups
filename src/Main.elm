57fb7898f06653694fef9f6ab76f5c455483b136
Nikolaus Wettstein <office@emptygraphics.com>
2019-10-01 22:43:02 +0200

    basic counter working

---
+++ src/Main.elm
@@ -0,0 +1,35 @@
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+main =
+    Browser.sandbox {
+        init = init,
+        update = update,
+        view = view }
+
+type alias Model = Int
+
+init : Model
+init = 0
+
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [onClick Decrement] [text "-1"]
+        , div [] [text <| String.fromInt model]
+        , button [onClick Increment] [text "+1"]
+        ]
+
