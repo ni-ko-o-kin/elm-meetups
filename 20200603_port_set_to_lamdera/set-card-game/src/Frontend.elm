0df74148984a7905edf191eaf795dd24cd6bd073
3nigm4 <3wb@protonmail.com>
2020-06-03 18:20:01 +0200

    Add frontend boilerplate

---
+++ 20200603_port_set_to_lamdera/set-card-game/src/Frontend.elm
@@ -0,0 +1,18 @@
+module Frontend exposing (app)
+
+import Lamdera
+
+app =
+    Lamdera.frontend
+        { init = init
+        , onUrlRequest = UrlClicked
+        , onUrlChange = UrlChanged
+        , update = update
+        , updateFromBackend = updateFromBackend
+        , subscriptions = subscriptions
+        , view =
+            \model ->
+                { title = "Lamdera Alpha"
+                , body = [ view model ]
+                }
+        }
