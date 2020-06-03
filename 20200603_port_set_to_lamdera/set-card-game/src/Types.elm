2eaa850387a09a049a29a2058cf45d94ee58d1e4
3nigm4 <3wb@protonmail.com>
2020-06-03 18:20:17 +0200

    Add types boilerplate

---
+++ 20200603_port_set_to_lamdera/set-card-game/src/Types.elm
@@ -0,0 +1,21 @@
+module Types exposing (..)
+
+type alias FrontendModel =
+  { message : String
+  }
+
+type alias BackendModel =
+  { message : String
+  }
+
+type FrontendMsg
+    = NoOpFrontendMsg
+
+type ToBackend
+    = NoOpToBackend
+
+type BackendMsg
+    = NoOpBackendMsg
+
+type ToFrontend
+    = NoOpToFrontend
